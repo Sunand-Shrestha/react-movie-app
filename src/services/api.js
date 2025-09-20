cc2b1acd1d86af98345df0c957e14884d7a76495
Sunand-Shrestha <sthasunand@gmail.com>
2025-09-20 17:14:04 +0545

    api exposed fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
-const API_KEY = "595dbe5adca80024d23babe4fd8798b4"
-const BASE_URL = "https://api.themoviedb.org/3"
-export const getpopularMovies = async () =>{
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
-    const data = await response.json()
-    return data.results
-}
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
+export const getpopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
 
-export const searchMovies = async (query) =>{
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
-    const data = await response.json()
-    return data.results
-}
+export const searchMovies = async (query) => {
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+  const data = await response.json();
+  return data.results;
+};
